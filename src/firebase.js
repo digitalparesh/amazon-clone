8d1318b8a3cea26c392e5e06b3f29201bd2b658a
digitalparesh <prshnandaniya@gmail.com>
2020-11-09 11:46:54 -0800

    added firebase and deleted

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCPtwJfEAVmy3L1iKa13wHCwtLGhRsTa0k",
+  authDomain: "challenge-fedbe.firebaseapp.com",
+  databaseURL: "https://challenge-fedbe.firebaseio.com",
+  projectId: "challenge-fedbe",
+  storageBucket: "challenge-fedbe.appspot.com",
+  messagingSenderId: "352549520951",
+  appId: "1:352549520951:web:a293d0b3e5f778851c21b2",
+  measurementId: "G-S38V1V80PY",
+};
